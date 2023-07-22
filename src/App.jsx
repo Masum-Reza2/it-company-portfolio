3b04a9603dba7de6bae04f055d69921684d5b975
Masum-Reza <careerhunter4280@gmail.com>
2023-07-22 16:45:14 -0700

    added sponsors section

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,19 @@ import Helping_hand from './components/Helping_hand'
 import Review from './components/Review'
 import Company_phrase from './components/Company_phrase'
 import Country from './components/Country'
+import Contact_us from './components/Contact_us'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Navbar/>
+    {/* <Navbar/>
     <Helping_hand/>
     <Review/>
     <Company_phrase/>
-    <Country/>
+    <Country/> */}
+    <Contact_us/>
     </>
   )
 }
---
+++ src/components/Contact_us.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+export default function Contact_us() {
+    return (
+        <>
+            <div className='space-y-5'>
+                
+                <h1 className='text-xl text-center mt-3 underline'>Contact Us</h1>
+                <h5 className='text-xl text-center mt-3'>demo@gmail.com</h5>
+                <div className=' w-1/2 m-auto'>
+                    <img className='w-[50%] m-auto' src="../images/mobile-iso_logo.svg" alt="" />
+                    <img className='w-3/2 m-auto' src="../images/mobile-Microsoft_partner.svg" alt="" />
+                </div>
+
+                <div className='text-2xl text-center space-x-5'>
+                <i class="fa-brands fa-facebook"></i>
+                <i class="fa-brands fa-linkedin"></i>
+                <i class="fa-brands fa-twitter"></i>
+                <i class="fa-brands fa-youtube"></i>
+                <i class="fa-brands fa-instagram"></i>
+                <i class="fa-brands fa-chrome"></i>
+                <i class="fa-brands fa-whatsapp"></i>
+                </div>
+
+            </div>
+        </>
+    )
+}
